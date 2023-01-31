ebfb13cb7171a5d9e77069f5ad7921574987a320
albanpuech <alban.puech@polytechnique.edu>
2023-01-31 10:46:44 +0100

    modified get dataset

--- env.py
+++ env.py
@@ -1,71 +1,66 @@
 import gym
 from gym import spaces
-import pygame
 import numpy as np
-import pandas as pd 
+import pandas as pd
 
 
-def get_dataset(path="data/european_wholesale_electricity_price_data_hourly.csv",country="Germany",use_cols=["Datetime (Local)","Price (EUR/MWhe)","Country"]) :
+def get_dataset(path="data/european_wholesale_electricity_price_data_hourly.csv", country="Germany", use_cols=["Datetime (Local)", "Price (EUR/MWhe)", "Country"]):
     df = pd.read_csv(path)
-    df = df[df.Country==country]
-    df.drop(["Country"], axis=1,inplace=True)
-    df.rename({"Datetime (Local)":"timestamp", "Price (EUR/MWhe)":"price"},inplace=True)
-    df.price = df.price/ 10 **6 
+    df = df[df.Country == country]
+    df.drop(["Country"], axis=1, inplace=True)
+    df.rename({"Datetime (Local)": "timestamp",
+              "Price (EUR/MWhe)": "price"}, inplace=True)
+    df.price = df.price / 10 ** 6
     return df
-    
+
 
 class GridWorldEnv(gym.Env):
 
-    def __init__(self, render_mode=None,df=None, k=5, NEC=10**5):
-        
+    def __init__(self, render_mode=None, df=None, k=5, NEC=10**5):
+
         self.NEC = NEC
         self.E1H = NEC/2
         self.df = df if df else get_dataset()
+        self.df["mean_price"] = self.df.price.rolling(self.k).mean()
         self.n_hours = len(self.df)
         self.k = k
         self.SOC = np.zeros(self.n_hours)
 
-        self.observation_space = spaces.Box(low=np.array([[0.0, -1] for i in range(k)]), high=np.array([[1.0, 2] for i in range(k)]), shape=(k,2),dtype=np.float32)
-    
+        self.observation_space = spaces.Box(low=np.array([[0.0, -1] for i in range(
+            k)]), high=np.array([[1.0, 2] for i in range(k)]), shape=(k, 2), dtype=np.float32)
+
         # We have 3 actions, corresponding to "charge, hold, discharge"
         self.action_space = spaces.Discrete(3)
 
     def _get_obs(self):
-      return np.hstack([df.loc[self.hour-self.k+1:self.hour+1,"price"],
-                               self.SOC[self.hour-self.k:self.hour]])
+        return np.hstack([self.df.loc[self.hour-self.k+1:self.hour+1, "price"],
+                          self.SOC[self.hour-self.k:self.hour]])
+
     def _get_info(self):
-      return self.df[self.hour]
+        return self.df[self.hour]
 
     def reset(self, seed=None, options=None):
-      self.hour = self.k
-      observation = self._get_obs()
-      info = self._get_info()
-      
-      return observation, info
+        self.hour = self.k
+        observation = self._get_obs()
+        info = self._get_info()
 
-    def step(self,action) :
-      if action == 0 :
-        self.SOC[self.hour] = max(0,  (self.SOC[self.hour-1]*self.NEC - self.E1H) / self.NEC)
-      
+        return observation, info
 
-      elif action == 1 :
-        self.SOC[self.hour] = self.SOC[self.hour-1]
-        
-      elif action == 2 :
-        self.SOC[self.hour] = min(1,  (self.SOC[self.hour-1]*self.NEC + self.E1H) / self.NEC)
+    def step(self, action):
+        if action == 0:
+            self.SOC[self.hour] = max(
+                0,  (self.SOC[self.hour-1]*self.NEC - self.E1H) / self.NEC)
 
-      reward = (self.action-1) * (self.df.mean_price[self.hour] - self.df.price[self.hour])
+        elif action == 1:
+            self.SOC[self.hour] = self.SOC[self.hour-1]
 
-      self.hour +=1
+        elif action == 2:
+            self.SOC[self.hour] = min(
+                1,  (self.SOC[self.hour-1]*self.NEC + self.E1H) / self.NEC)
 
+        reward = (self.action-1) * \
+            (self.df.mean_price[self.hour] - self.df.price[self.hour])
 
-      return self._get_obs(), reward, (self.hour == self.n_hours)
+        self.hour += 1
 
-      
-      
-
-        
-      
-
-
-    
+        return self._get_obs(), reward, (self.hour == self.n_hours)
